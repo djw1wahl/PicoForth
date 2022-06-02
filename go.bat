ed77760cdab8603561b8bc2c565a76b2060197e7
djw1wahl <dwahl@psl.wisc.edu>
2022-06-02 11:58:58 -0500

    Delete go.bat

--- go.bat
+++
@@ -1,6 +0,0 @@
-gcc -c forth.c
-gcc -c StackPrimitives.c
-gcc -c SystemPrimitives.c
-gcc -c Utilities.c
-gcc -c VFM_Build_Primitives.c
-gcc -o 4th.exe forth.o StackPrimitives.o  SystemPrimitives.o Utilities.o VFM_Build_Primitives.o
